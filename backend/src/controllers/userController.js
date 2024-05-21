2f5eae3bb60a2ddc465963b0bca2e4f942f1fd92
Long Nguyen <npl.long2001@gmail.com>
2024-05-21 19:06:53 +1000

    add login/logout/register pages and functionalities

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -8,43 +8,42 @@ exports.register = async (req, res) => {
   const { username, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10); // Hash the password with a salt of 10 rounds
-    await db('users').insert({
+    await db("users").insert({
       username,
       password: hashedPassword,
     });
-    res.status(201).json({ message: 'User registered successfully' }); // Respond with success message
+    res.status(201).json({ message: "User registered successfully" }); // Respond with success message
   } catch (error) {
     res.status(500).json({ error: error.message, message: "Duplicated user" }); // Handle any errors
   }
 };
 
-
 // Authenticate user and return JWT
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await db('users').where({ username }).first(); // Find user by username
-    if (!user || !(await bcrypt.compare(password, user.password))) { // Check if user exists and passwords match
-      return res.status(401).json({ message: 'Invalid credentials' }); // Respond with error if credentials are invalid
+    const user = await db("users").where({ username }).first(); // Find user by username
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      // Check if user exists and passwords match
+      return res.status(401).json({ message: "Invalid credentials" }); // Respond with error if credentials are invalid
     }
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     }); // Generate JWT
     res.json({ message: "Login successful", token }); // Respond with JWT
   } catch (error) {
-    res.status(500).json({ error: error.message, message: "Login failed"}); // Handle any errors
+    res.status(500).json({ error: error.message, message: "Login failed" }); // Handle any errors
   }
 };
 
 exports.getUser = async (req, res) => {
   try {
-    const user = await db('users').where({ id: req.user.id }).first(); // Find user by ID
+    const user = await db("users").where({ id: req.user.id }).first(); // Find user by ID
     if (!user) {
-      return res.status(404).json({ message: 'User not found' }); // If user is not found, respond with 404
+      return res.status(404).json({ message: "User not found" }); // If user is not found, respond with 404
     }
     res.json(user); // Respond with user data
   } catch (error) {
     res.status(500).json({ error: error.message }); // Handle any errors
   }
 };
-
